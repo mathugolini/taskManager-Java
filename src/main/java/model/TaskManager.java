585db7d20ff333f1ad78c87411e07a70451c2d48
Matheus <mathugolini@gmail.com>
2023-08-02 14:11:28 -0300

    Implementando classe model

--- src/main/java/model/TaskManager.java
+++ src/main/java/model/TaskManager.java
@@ -1,4 +1,23 @@
+package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "taskManager")
 public class TaskManager {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(unique = true)
+    String description;
+    @Column(unique = true)
+    Date dueDate;
+    Boolean completed;
 }
